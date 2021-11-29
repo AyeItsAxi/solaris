58662e09ce8f911eeb823feab8e0a56fa1fef1a9
shawn <tristangee1@gmail.com>
2021-11-29 11:02:40 -0700

    useless stuff

---
+++ helpexamp/placeholder.js
@@ -0,0 +1,2 @@
+//this does nothing right now, i need to get this working in a bit. 
+//might just add these to the commands folder tbh, too lazy to add the code to make the other ones go to a seperate folder
